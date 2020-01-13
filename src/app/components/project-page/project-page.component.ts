c1189bf0f0bf17755053e3b1ee69afb368212859
mihaellaluca <48389197+mihaellaluca@users.noreply.github.com>
2020-01-13 03:44:20 +0200

    Suggested users completed

--- src/app/components/project-page/project-page.component.ts
+++ src/app/components/project-page/project-page.component.ts
@@ -1,79 +1,92 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Project } from 'src/app/models/CreateProjectModel';
-import { ProjectTabService } from 'src/app/services/project-tab.service';
-import { UserService } from 'src/app/services/user-service.service';
-import { UserModel } from 'src/app/models/UserModel';
-import { InvitationModel } from 'src/app/models/InvitationModel';
-import { GlobalService } from 'src/app/services/global-service.service';
-import { InvitationsService } from 'src/app/services/invitations.service';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { Component, OnInit, Input } from "@angular/core";
+import { Project } from "src/app/models/CreateProjectModel";
+import { ProjectTabService } from "src/app/services/project-tab.service";
+import { UserService } from "src/app/services/user-service.service";
+import { UserModel } from "src/app/models/UserModel";
+import { InvitationModel } from "src/app/models/InvitationModel";
+import { GlobalService } from "src/app/services/global-service.service";
+import { InvitationsService } from "src/app/services/invitations.service";
+import { ActivatedRoute, Router, ParamMap } from "@angular/router";
+import { switchMap } from "rxjs/operators";
 
 @Component({
-	selector: 'app-project-page',
-	templateUrl: './project-page.component.html',
-	styleUrls: [ './project-page.component.scss' ]
+  selector: "app-project-page",
+  templateUrl: "./project-page.component.html",
+  styleUrls: ["./project-page.component.scss"]
 })
 export class ProjectPageComponent implements OnInit {
-	userId: string = localStorage.getItem('userId');
-	projectId: string = ''; // trebuie luat din linkul curent. /project-page/projectId
-	project: Project = new Project();
-	neededTech: string[] = [];
-	userIsOwner: boolean = false;
-	collaborators: string[] = [];
-	suggestedUsers: UserModel[];
+  userId: string = localStorage.getItem("userId");
+  projectId: string = ""; // trebuie luat din linkul curent. /project-page/projectId
+  project: Project = new Project();
+  neededTech: string[] = [];
+  userIsOwner: boolean = false;
+  collaborators: string[] = [];
+  suggestedUsers: UserModel[];
   userIsCollaborator: boolean = false;
+  cardInfos;
 
-	constructor(
-		private projectService: ProjectTabService,
-		private userService: UserService,
-		private invitationService: InvitationsService,
-		private globalService: GlobalService,
-		private route: ActivatedRoute,
-		private router: Router
-	) {}
+  constructor(
+    private projectService: ProjectTabService,
+    private userService: UserService,
+    private invitationService: InvitationsService,
+    private globalService: GlobalService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-	ngOnInit() {
-		document.body.classList.add('bg-img-home');
-		this.route.paramMap
-			.pipe(switchMap((params: ParamMap) => params.get('id')))
-			.subscribe((data) => (this.projectId += data));
+  ngOnInit() {
+    document.body.classList.add("bg-img-home");
+    this.route.paramMap
+      .pipe(switchMap((params: ParamMap) => params.get("id")))
+      .subscribe(data => (this.projectId += data));
 
-		console.log('proiect id:', this.projectId);
-		this.projectService.getProjectById(this.projectId).pipe().subscribe(
-			(data) => {
-				console.log('Project aaaaaaaa: ', data);
+    console.log("proiect id:", this.projectId);
+    this.projectService
+      .getProjectById(this.projectId)
+      .pipe()
+      .subscribe(
+        data => {
+          console.log("Project aaaaaaaa: ", data);
 
-				for (var index in data[0].collaboratorsId) {
-					this.userService.getUserById(data[0].collaboratorsId[index]).pipe().subscribe(
-						(data) => {
-							this.collaborators.push(data['firstName'] + ' ' + data['lastName']);
-						},
-						(error) => {
-							console.log(error);
-						}
-					);
-				}
-				console.log('Colaboratori: ', this.collaborators);
-				this.project = {
-					projectId: data[0].id,
-					name: data[0].name,
-					ownerId: data[0].ownerId,
-					ownerName: '',
-					description: data[0].description,
-					technologies: data[0].technologies,
-					state: data[0].state
-				};
-				this.userService.getUserById(this.project.ownerId).pipe().subscribe(
-					(data) => {
-						this.project.ownerName = data['firstName'] + ' ' + data['lastName'];
-					},
-					(error) => {
-						console.log(error);
-					}
-				);
+          for (var index in data["collaboratorsId"]) {
+            this.userService
+              .getUserById(data["collaboratorsId"][index])
+              .pipe()
+              .subscribe(
+                data => {
+                  this.collaborators.push(
+                    data["firstName"] + " " + data["lastName"]
+                  );
+                },
+                error => {
+                  console.log(error);
+                }
+              );
+          }
+          console.log("Colaboratori: ", this.collaborators);
+          this.project = {
+            projectId: data["id"],
+            name: data["name"],
+            ownerId: data["ownerId"],
+            ownerName: "",
+            description: data["description"],
+            technologies: data["technologies"],
+            state: data["state"]
+          };
+          this.userService
+            .getUserById(this.project.ownerId)
+            .pipe()
+            .subscribe(
+              data => {
+                this.project.ownerName =
+                  data["firstName"] + " " + data["lastName"];
+              },
+              error => {
+                console.log(error);
+              }
+            );
 
-				this.neededTech = this.project.technologies;
+          this.neededTech = this.project.technologies;
 
           this.userService
             .getSuggestedUsers(this.neededTech)
@@ -81,6 +94,7 @@ export class ProjectPageComponent implements OnInit {
             .subscribe(
               data => {
                 console.log("Suggested users: ", data);
+                this.cardInfos = data;
               },
               error => {
                 console.log(error);
@@ -95,16 +109,16 @@ export class ProjectPageComponent implements OnInit {
       );
   }
 
-	joinProject() {
-		// add current user to project
-		let invitation = new InvitationModel(
-			this.projectId,
-			localStorage.getItem('userId'),
-			this.project.ownerId,
-			this.globalService.InvitationUserToOwner
-		);
+  joinProject() {
+    // add current user to project
+    let invitation = new InvitationModel(
+      this.projectId,
+      localStorage.getItem("userId"),
+      this.project.ownerId,
+      this.globalService.InvitationUserToOwner
+    );
 
-		console.log(invitation);
+    console.log(invitation);
 
     this.invitationService
       .joinProject(invitation)
@@ -112,14 +126,14 @@ export class ProjectPageComponent implements OnInit {
       .subscribe(
         data => {
           console.log("After: ", data);
-          window.alert("Your request is in pending! Please wait for the owner to accept it.");
+          window.alert(
+            "Your request is in pending! Please wait for the owner to accept it."
+          );
         },
         error => {
           console.log(error);
         }
       );
-
-    
   }
 
   checkUserIsOwner() {
--- src/app/components/users-suggestions/users-suggestions.component.ts
+++ src/app/components/users-suggestions/users-suggestions.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-users-suggestions',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersSuggestionsComponent implements OnInit {
 
-  constructor() { }
+  @Input() card;
+	name: string = '';
+  technologies: string = '';
+  
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    console.log("THIS.CARD:",this.card);
+		this.userService.getUserById(this.card.userId).pipe().subscribe(
+			(data) => {
+				console.log(data);
+				this.name = data['firstName'] + ' ' + data['lastName'];
+				data['knownTechnologies'].forEach((element) => {
+					this.technologies = this.technologies + ' ' + element;
+				});
+			},
+			(error) => {
+				console.log('plpl');
+			}
+		);
   }
 
 }
